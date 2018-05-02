0a5f99848492d0829cde93b75ec9390561e37aa2
Munkhdorj Baatarsuren <Muugii-bs@users.noreply.github.com>
2018-05-02 11:13:10 +0900

    update env.sh

--- env.sh
+++ env.sh
@@ -6,9 +6,11 @@ cd configs
 mv gitconfig ~/.gitconfig
 git checkout -b vim-full origin/vim-full-config
 cd vim-full-config
-rm -rf ~/.vim 
+rm -rf ~/.vim
 mv vim ~/.vim
 mv vimrc ~/.vimrc
 cd ../../
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer || sudo apt-get -y install build-essential cmake || sudo apt-get -y install build-essential cmake3 || ./install.py --clang-completer |
 rm -rf configs
 rm -rf .git
