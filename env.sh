5710d13151c23f4a9066f0a997b3f481f0fc5ced
Munkhdorj Baatarsuren <Muugii-bs@users.noreply.github.com>
2018-05-09 15:41:55 +0900

    update env

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-# ZSH INSTALL 
+# ZSH INSTALL
 sudo apt-get -y update
 sudo apt-get -y install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -9,12 +9,17 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
   brew install neovim
 } || {
   sudo apt-get install software-properties-common
-  sudo apt-get install python-software-properties 
+  sudo apt-get install python-software-properties
   sudo add-apt-repository ppa:neovim-ppa/stable
   sudo apt-get update
   sudo apt-get install neovim
   sudo apt-get install python-dev python-pip python3-dev python3-pip
 }
+pip2 install --upgrade pip
+pip3 install --upgrade pip
+pip2 install --user --upgrade neovim
+pip3 install --user --upgrade neovim
+nvim -c :PlugInstall -c :q! -c :q!
 
 git clone https://github.com/Muugii-bs/configs.git
 cd configs
@@ -27,11 +32,13 @@ rm -rf ~/.vim
 mv vim ~/.vim
 mv vimrc ~/.vimrc
 
-mkdir ~/.config/nvim 
+mkdir ~/.config/nvim
 mv init.vim ~/.config/nvim/
-cp -r ~/.vim/colors ~/.config/nvim/ 
+cp -r ~/.vim/colors ~/.config/nvim/
 cd ../../
 rm -rf configs
 rm -rf .git
 cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer || sudo apt-get -y install build-essential cmake || sudo apt-get -y install build-essential cmake3 || ./install.py --clang-completer
+cd ~/.local/share/nvim/plugged/YouCompleteMe
 ./install.py --clang-completer || sudo apt-get -y install build-essential cmake || sudo apt-get -y install build-essential cmake3 || ./install.py --clang-completer
