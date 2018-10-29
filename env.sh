f68451f87b7decdb736bad4a7753b3334edd1b48
Mugi-da <munkhdorj@data-artist.com>
2018-10-29 14:35:44 +0900

    minor change to env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 cp ./zshrc ~/.zshrc
-
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # GIT & VIM CONFIGS
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
